142edf21830dfad09d1f1181126c14922bbc2d5f
Kevin Kwok <antimatter15@gmail.com>
2018-06-14 17:32:11 -0700

    updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,6 @@ cp -r image dist
 cp index.html dist
 sed "s/CURRENT_DATE/$(date)/g" template.appcache > dist/offline.appcache
 cp styles.css dist
+echo "offlinewiki.app" > dist/CNAME
+gh-pages -d dist
+echo "Deployed to https://offlinewiki.app/"
